2443554d17a692a1797e6ae210c6c20fc6ef2694
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-10-21 13:15:53 +0200

    modified user.cs

--- classes/User.cs
+++ classes/User.cs
@@ -8,14 +8,7 @@ public class User
     public string Email { get; set; }
     public string Password { get; set; }
     
-
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string Region { get; set; }
-    public int PostalCode { get; set; }
-
     public int PersonalNumber { get; set; }
-
 
     public List<Account> Accounts { get; set; }
 
@@ -24,11 +17,6 @@ public class User
 
 
 
-    public User()
-    {
-        Accounts = new List<Account>();
-
-       
         
     }
 
@@ -40,7 +28,7 @@ public class User
 
     
 
-}
+
 
 
 
