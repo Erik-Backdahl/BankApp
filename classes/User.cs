de9f7b2aac7b4a6f454ec3d64327b1a90448805c
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-10-21 11:58:06 +0200

    modified user.cs class

--- classes/User.cs
+++ classes/User.cs
@@ -1,4 +1,47 @@
-class User
+
+
+using BankApp.classes;
+
+public class User
 {
+   public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    
+
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public int PostalCode { get; set; }
+
+    public int PersonalNumber { get; set; }
+
+
+    public List<Account> Accounts { get; set; }
+
+
+
+
+
+
+    public User()
+    {
+        Accounts = new List<Account>();
+
+       
+        
+    }
+
+
+
+
+
+
+
+    
 
 }
+
+
+
+
