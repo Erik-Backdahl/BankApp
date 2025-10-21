2d74e5c8a0153cdd6a602ca81140036359bed9e1
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-10-21 11:53:30 +0200

    Crated account class

---
+++ classes/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.classes
+{
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string ownerid { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; }
+        public string Currency { get; set; }
+
+
+
+        
+
+
+
+
+
+
+    }
+}
