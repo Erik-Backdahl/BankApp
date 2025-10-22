cbf13f14a8245a88c32815a110bc0c8980b3fb8c
momimaga <momimaga60@gmail.com>
2025-10-22 13:11:13 +0200

    la till withdraw

---
+++ classes/Withdraw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.classes
+{
+    internal class muhammad
+    {
+        public static void Withdraw(User currentUser)
+        {
+            Console.WriteLine("\nEnter your account number:");
+            int index = 0;
+            foreach (var account in currentUser.Accounts)
+            {
+                Console.WriteLine($"({index}) " + account.AccountNumber);
+                index++;
+            }
+
+        }
+    }
+}
