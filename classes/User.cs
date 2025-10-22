4e55a1393402b4fdba69bcada2792115e45d1e77
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-10-22 15:18:20 +0200

    added construoer for user class

--- classes/User.cs
+++ classes/User.cs
@@ -9,24 +9,35 @@ public class User
     public string Password { get; set; }
     
     public int PersonalNumber { get; set; }
-
+    public string Address { get; set; }
     public List<Account> Accounts { get; set; }
 
+    public User(string name, string email, string password, int personalNumber, string address)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        PersonalNumber = personalNumber;
+        Address = address;
+        Accounts = new List<Account>();
 
 
 
 
 
-        
+
     }
 
 
+}
 
 
 
 
 
-    
+
+
+
 
 
 
