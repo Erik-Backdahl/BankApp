14a36152520968ecf8d595cff4553c42877d9560
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-10-24 03:29:49 +0200

    jj

--- classes/Admin.cs
+++ classes/Admin.cs
@@ -1,4 +1,23 @@
-class Admin : User
+ public class Admin 
+
+
 {
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+
+
+    public Admin(string username, string password, string role)
+    {
+        Username = username;
+        Password = password;
+        Role = role;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Admin Username: {Username}, Role: {Role}");
+    }
+
+
 
 }
