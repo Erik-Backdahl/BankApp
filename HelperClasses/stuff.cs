d43e6b2c30265393c3ad35c4bbe53519190c7524
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-10-28 13:53:25 +0100

    Added admin user method (add/remove

---
+++ HelperClasses/stuff.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.HelperClasses
+{
+
+
+    public class Admin_user_add
+
+    {
+
+    }
+
+    public class Admin_user
+    {
+
+
+        // method to add new admin user
+
+        public static void AddAdminUser()
+        {
+            Console.WriteLine("Lägg till ny administratörsanvändare");
+            Console.WriteLine("Ange användarnamn:");
+            string username = GetUserInput.ValidateString();
+            Console.WriteLine("Ange lösenord:");
+            string password = GetUserInput.ValidateString();
+
+            Console.WriteLine(" ange rollen ");
+            string role = GetUserInput.ValidateString();
+
+
+            Menu.AllUsers.Add(new User { Name = username, Password = password, Email = "", PersonalNumber = 0, Administator = true });
+
+            Console.WriteLine($"Ny administratörsanvändare '{username}' med rollen '{role}' har lagts till.");
+        }
+
+
+        // method to remove admin user
+        public static void RemoveAdminUser()
+        {
+            Console.WriteLine("Ange användarnamn för administratörsanvändare som ska tas bort:");
+            string username = GetUserInput.ValidateString();
+
+            var user = Menu.AllUsers.Find(u => u.Name == username && u.Administator);
+            if (user != null)
+            {
+                Menu.AllUsers.Remove(user);
+                Console.WriteLine($"Administratörsanvändare '{username}' har tagits bort.");
+            }
+            else
+            {
+                Console.WriteLine($"Ingen administratörsanvändare med namnet '{username}' hittades.");
+            }
+        }
+    }
+}
