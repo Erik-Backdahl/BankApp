ce02686bf395426d5e54aba691c8762d0fe97ccc
Mustafa Alikhadyer <lordod77@gmail.com>
2025-11-06 16:51:12 +0100

    while loop added in takeloan

--- StaticClasses/TakeLoan.cs
+++ StaticClasses/TakeLoan.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using BankApp.classes;
 
 static class TakeLoan
@@ -8,32 +7,48 @@ static class TakeLoan
     {
         Console.WriteLine("=== Loan Application ===");
 
-        Console.Write("Enter loan amount: ");
-        decimal loanAmount = Convert.ToDecimal(Console.ReadLine());
-
-         if (loanAmount <= 0)
+        decimal loanAmount;
+        while (true)
         {
-            Console.WriteLine("Loan amount must be greater than zero.");
-            return;
+            Console.Write("Enter loan amount: ");
+            if (decimal.TryParse(Console.ReadLine(), out loanAmount))
+            {
+                if (loanAmount > 0)
+                    break;
+                else
+                    Console.WriteLine("Loan amount must be greater than zero.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
         }
 
-        Console.Write("Enter loan duration in years: ");
-        int LoanInYears = Convert.ToInt32(Console.ReadLine());
+        int LoanInYears;
+        while (true)
+        {
+            Console.Write("Enter loan duration in years: ");
+            if (int.TryParse(Console.ReadLine(), out LoanInYears))
+            {
+                if (LoanInYears > 0)
+                    break;
+                else
+                    Console.WriteLine("Loan duration must be at least 1 year.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a whole number.");
+            }
+        }
 
         decimal interestRate = 0.05m; // 5% interest rate
 
-        if (LoanInYears <= 0)
-        {
-            Console.WriteLine("Loan duration must be at least 1 year.");
-            return;
-        }
-
         account.Balance += loanAmount;
 
-        decimal totalDebt = loanAmount * (1 + loanAmount * interestRate);
+        decimal totalDebt = loanAmount * (1 + interestRate * LoanInYears);
 
-        Console.WriteLine($"\nYou have taken a loan of {loanAmount} SEK at {interestRate * 100}% interest.");
-        Console.WriteLine($"You will need to repay {totalDebt} SEK after 1 year.");
+        Console.WriteLine($"\nYou have taken a loan of {loanAmount} SEK at {interestRate * 100}% interest per year.");
+        Console.WriteLine($"You will need to repay {totalDebt} SEK after {LoanInYears} year(s).");
         Console.WriteLine($"New account balance: {account.Balance} SEK");
     }
 }
