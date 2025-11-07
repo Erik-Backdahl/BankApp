8eca06d461932dba39bd1fc80ce2a0bb0f68b69d
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2025-11-07 22:58:00 +0100

    hhhh

---
+++ StaticClasses/AdminStatistics.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BankApp.StaticClasses
+{
+   public class AdminStatistics
+    {
+
+        private List<Transaction> transactions;
+
+
+
+        public AdminStatistics(List<Transaction> transactions) 
+        {
+            this.transactions = transactions;
+        }
+
+
+
+        public void ShowStatistics()
+
+        {
+
+
+            Console.WriteLine("\n========== STATISTIK ==========");
+
+            // 1. Totalt antal transaktioner
+            int numberoftransactions = transactions.Count;
+            Console.WriteLine($"Total number of transactions:: {numberoftransactions}");
+
+            //Total amount transferred//
+
+            decimal totalBelopp = 0;
+            foreach (var t in transactions)
+            {
+                totalBelopp += t.Amount;
+            }
+
+
+
+
+        }
+
+
+    }
+    
+}
